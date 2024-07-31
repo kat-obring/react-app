8838f11835a70cdc334d7b89b82aabf260e10173
Kat Obring <katja.obring@gmail.com>
2024-07-31 11:59:22 +0100

    regex adding more flexibility

--- e2e/src/step-definitions/assertions/verify-elements-visibility.ts
+++ e2e/src/step-definitions/assertions/verify-elements-visibility.ts
@@ -2,14 +2,14 @@ import { Then } from '@cucumber/cucumber'
 import { expect } from '@playwright/test'
 
 Then(
-    /^the "([^"]*)" should contain the text "([^"]*)"$/,
+    /^the "([^"]*)" should contain the text "(.*)"$/,
    async function(elementKey: string, expectedElementText: string) {
 
         console.log(`the ${elementKey} should contain the text ${expectedElementText}`)
 
         const content = await global.page.textContent("[data-id='contacts']")
 
-        expect(content).toBe('Contacts')
+        expect(content).toBe(expectedElementText)
 
     }
 )
