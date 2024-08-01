3d59c5f0efbf61185ff3e6f35090cff21cc8bc40
Kat Obring <katja.obring@gmail.com>
2024-08-01 11:43:47 +0100

    adding data-id for homepage header logo and test to verify visibility

--- e2e/src/features/home-page.feature
+++ e2e/src/features/home-page.feature
@@ -6,4 +6,5 @@ Feature: As a user I expect to be able to navigate to the home page
 Scenario: As a user I expect to be able to see contacts
     Given I am on the "home" page
     Then the "contacts header" should contain the text "Contacts"
+    And the "header logo" should be displayed
 
--- e2e/src/step-definitions/assertions/verify-elements-visibility.ts
+++ e2e/src/step-definitions/assertions/verify-elements-visibility.ts
@@ -13,3 +13,12 @@ Then(
 
     }
 )
+
+Then(
+    /^the "([^"]*)" should be displayed$/,
+    async function(elementKey: string) {
+        console.log('the ${elementKey} should be displayed')
+        const locator = global.page.locator("[data-id='header-logo']")
+        await expect(locator).toBeVisible
+    }
+)
