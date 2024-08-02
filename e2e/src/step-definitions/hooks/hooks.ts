b9c156d3dc5d6b7056d4867a8147a3ce5f6981cc
Kat Obring <katja.obring@gmail.com>
2024-08-02 10:09:29 +0100

    adding video and screenshot on failure

--- e2e/src/step-definitions/hooks/hooks.ts
+++ e2e/src/step-definitions/hooks/hooks.ts
@@ -1,4 +1,4 @@
-import { BeforeAll, Before, AfterAll, After } from "@cucumber/cucumber";
+import { BeforeAll, Before, AfterAll, After, Status } from "@cucumber/cucumber";
 const { chromium } = require("playwright");
 
 BeforeAll(async () => {
@@ -11,11 +11,24 @@ AfterAll(async () => {
     await global.browser.close();
 });
 
-Before(async () =>{
-    global.context = await global.browser.newContext();
+Before(async (scenario) =>{
+    global.context = await global.browser.newContext({
+        recordVideo: {
+            dir: './reports/video/'+scenario.pickle.name,
+        }
+    });
+
     global.page = await global.context.newPage();
 });
 
-After(async () =>{
-    await global.page.close();
+After(async (scenario) =>{
+    const scenarioStatus = scenario.result?.status;
+        if (scenarioStatus === 'FAILED') {
+            await global.page.screenshot({
+                path: `./reports/screenshots/${scenario.pickle.name}.png`
+            })
+        }
+
+
+        await global.page.close();
 });
